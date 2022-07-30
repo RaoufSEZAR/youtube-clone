da17d98fbf8ebece1f5c0c3745944b5d51c92fcf
raoufsezar <raoufsato999@gmail,com>
2022-07-30 20:07:13 +0300

    add jwt verification

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,4 +1,25 @@
-export const update = async (req, res, next) => {};
+import User from "../models/User.js";
+import { createError } from "./../error.js";
+
+export const update = async (req, res, next) => {
+	if (req.params.id === req.user.id) {
+		try {
+			const updatedUser = await User.findByIdAndUpdate(
+				req.params.id,
+				{
+					$set: req.body,
+				},
+				{ new: true }
+			);
+
+			res.status(200).json(updatedUser);
+		} catch (err) {
+			next(err);
+		}
+	} else {
+		return next(createError(403, "You can update only your account!"));
+	}
+};
 export const deleteUser = async (req, res, next) => {};
 export const getUser = async (req, res, next) => {};
 export const subscribe = async (req, res, next) => {};
